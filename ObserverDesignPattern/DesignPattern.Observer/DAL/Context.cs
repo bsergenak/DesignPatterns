fa858eed6f81b83bf435f159191d30ee5abe7a55
bsergenak <berkaysergenak@hotmail.com>
2023-09-06 03:09:02 +0300

    ObserverDesignPattern

---
+++ ObserverDesignPattern/DesignPattern.Observer/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Observer.DAL
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=sego\\SQLEXPRESS;initial catalog=DesingPattern4;integrated security=true;");
+        }
+        public DbSet<WelcomeMessage> WelcomeMessages { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<UserProcess> UserProcesses { get; set; }
+    }
+}
