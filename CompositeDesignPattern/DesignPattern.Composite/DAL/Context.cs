db59c73cf4447d34756e6296a88361afcdd707d7
bsergenak <berkaysergenak@hotmail.com>
2023-09-08 01:58:17 +0300

    CompositeDesignPattern

---
+++ CompositeDesignPattern/DesignPattern.Composite/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Composite.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=sego\\SQLEXPRESS;initial catalog=DesingPattern7;integrated security=true;");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
