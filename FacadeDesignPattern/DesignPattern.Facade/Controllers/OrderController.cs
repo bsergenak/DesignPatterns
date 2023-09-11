11fb90a37ed55025e2cde54e1bef9e047d9a547f
bsergenak <berkaysergenak@hotmail.com>
2023-09-11 04:26:45 +0300

    FacadeDesignPattern

---
+++ FacadeDesignPattern/DesignPattern.Facade/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using DesignPattern.Facade.DAL;
+using DesignPattern.Facade.FacadePattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.Facade.Controllers
+{
+    public class OrderController : Controller
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult OrderDetailStart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OrderDetailStart(int customerId, int productId, int orderId, int productCount, decimal productPrice)
+        {
+            OrderFacade order = new OrderFacade();
+            order.CompleteOrderDetail(customerId, productId, orderId, productCount, productPrice);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult OrderStart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OrderStart(int customerID)
+        {
+            OrderFacade orderFacade = new OrderFacade();
+            orderFacade.CompleteOrder(customerID);
+            return RedirectToAction("Index");
+        }
+    }
+}
